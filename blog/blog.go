736c5f7429a683d2029c20b4e6e12292209fc954
Setter <setter.guo@gmail.com>
2016-09-08 09:24:14 +0800

    Make go-blog feasible for our requirements.

--- blog/blog.go
+++ blog/blog.go
@@ -7,6 +7,8 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"net/http"
 	"strings"
 	"time"
@@ -19,15 +21,23 @@ import (
 
 const hostname = "blog.golang.org" // default hostname for blog server
 
-var config = blog.Config{
-	Hostname:     hostname,
-	BaseURL:      "//" + hostname,
-	GodocURL:     "//golang.org",
-	HomeArticles: 5,  // articles to display on the home page
-	FeedArticles: 10, // articles to include in Atom and JSON feeds
-	PlayEnabled:  true,
-	FeedTitle:    "The Go Programming Language Blog",
-}
+var (
+	httpAddr = flag.String("http", "localhost:8080", "HTTP listen address")
+	barContentPath = flag.String("bar", "bar/", "path to bar content files")
+	contentPath = flag.String("content", "content/", "path to content files")
+	templatePath = flag.String("template", "template/", "path to template files")
+	staticPath = flag.String("static", "static/", "path to static files")
+
+	config = blog.Config{
+		Hostname:     hostname,
+		BaseURL:      "//" + hostname,
+		GodocURL:     "//golang.org",
+		HomeArticles: 5, // articles to display on the home page
+		FeedArticles: 10, // articles to include in Atom and JSON feeds
+		PlayEnabled:  true,
+		FeedTitle:    "Weekend Chinese Tech Meetup",
+	}
+)
 
 func init() {
 	// Redirect "/blog/" to "/", because the menu bar link is to "/blog/"
@@ -49,4 +59,36 @@ func staticHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.ServeContent(w, r, name, time.Time{}, strings.NewReader(b))
+}
+
+func serveBlog(prefix string, contentPath string) {
+	blogConfig := blog.Config{
+		Hostname: hostname,
+		BaseURL: "//" + hostname,
+		HomeArticles: 5,
+		FeedArticles: 10,
+		PlayEnabled: true,
+		FeedTitle: "unimportant",
+		ContentPath: contentPath,
+		TemplatePath: *templatePath,
+	}
+
+	s2, err := blog.NewServer(blogConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.Handle(prefix + "/", http.StripPrefix(prefix, s2))
+}
+
+func main() {
+	flag.Parse()
+
+	serveBlog("/bar", *barContentPath)
+	serveBlog("", *contentPath)
+
+	log.Printf("xfguo: hello world\n")
+
+	fs := http.FileServer(http.Dir(*staticPath))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	log.Fatal(http.ListenAndServe(*httpAddr, nil))
 }
